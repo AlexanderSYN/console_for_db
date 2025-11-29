8fbf9257389500f4f5d4c090c9a8939ffebbd183
Alexander129 <000777000casa@gmail.com>
2025-11-30 02:00:18 +0300

    added a sql query for SELECT

--- src/main/java/console/core_console.java
+++ src/main/java/console/core_console.java
@@ -1,6 +1,12 @@
 package console;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 import java.util.Scanner;
+
+import static DB.util.*;
 
 public class core_console {
 
@@ -8,27 +14,49 @@ public class core_console {
 
     protected static boolean isRun = true;
     protected static String input_from_console;
-
+    protected static String sql_query;
 
     public static void start_console() {
-        while (isRun) {
-            System.out.println("\033[H\033[2J"); // cleared console
-            System.out.println("for help type help");
+        try {
+            while (isRun) {
+                System.out.println("\033[H\033[2J"); // cleared console
+                System.out.println("for help type help");
+                System.out.print("# ");
+                input_from_console = in.nextLine().toLowerCase();
+
+                if (input_from_console.equals("help")) {
+                    System.out.println("help -> for help");
+                    System.out.println("cls / clear -> for clean console");
+                    System.out.println("ex / exit -> for exit from console");
+                } else if (input_from_console.equals("cls") || input_from_console.equals("clear")) {
+                    System.out.println("\033[H\033[2J");
+                } else if (input_from_console.equals("sql")) {
+
+                    System.out.println("SQL MODE (please enter sql query): ");
+                    sql_query = in.nextLine();
+
+                    Connection con = get_connection();
+                    Statement statement = con.createStatement();
+
+                    ResultSet resultSet = statement.executeQuery(sql_query);
+                    ResultSetMetaData metaData = resultSet.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+
+                    while (resultSet.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.println(metaData.getColumnName(i) + " - " + resultSet.getString(i) + "\t");
+                        }
+                        System.out.println("\n");
+                    }
+
+                } else if (input_from_console.equals("ex") || input_from_console.equals("exit")) {
+                    isRun = false;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e);
+            System.out.println("!for help type help!");
             System.out.print("# ");
-            input_from_console = in.nextLine().toLowerCase();
-
-            if (input_from_console.equals("help")) {
-                System.out.println("help -> for help");
-                System.out.println("cls / clear -> for clean console");
-                System.out.println("ex / exit -> for exit from console");
-            }
-            else if (input_from_console.equals("cls") || input_from_console.equals("clear")) {
-                System.out.println("\033[H\033[2J");
-            }
-            else if (input_from_console.equals("ex") || input_from_console.equals("exit")) {
-                isRun = false;
-            }
-
         }
     }
 }
