70047e84d528cf51803982a362b7acaa643bc017
Alexander129 <000777000casa@gmail.com>
2025-12-07 02:12:41 +0300

    added a command for writing more than 1 sql query

--- src/main/java/console/helper_console.java
+++ src/main/java/console/helper_console.java
@@ -1,13 +1,156 @@
 package console;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 import static DB.util.*;
 
 public class helper_console {
+
+    /**
+     *
+     * for executing sql queries (for !one line)
+     *
+     * @param sql_queries -> the array where all sql queries are stored
+     * @param count_request -> number of requests
+     *
+     */
+    public static void exec_sql_queries(String[] sql_queries, int count_request) {
+
+       try {
+           // main variables for sql queries
+           Connection con = get_connection();
+           Statement statement = con.createStatement();
+
+           int rows_affected = 0;
+
+           for (int i = 0; i <= count_request; i++) {
+               if (sql_queries[i] == null) continue;
+               System.out.println("name query: " + sql_queries[i] + "\nnumber: " + i);
+
+               if (sql_queries[i].length() < 6) {
+                   System.out.println("please enter correctly sql query!");
+               }
+               //======================
+               // commands via SELECT
+               //======================
+               else if (sql_queries[i].toLowerCase().substring(0, 6).equals("select")) {
+                   long start_time = System.nanoTime();
+
+                   // executing sql query
+                   ResultSet result_set = statement.executeQuery(sql_queries[i]);
+                   ResultSetMetaData meta_data = result_set.getMetaData();
+                   int columnCount = meta_data.getColumnCount();
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   while (result_set.next()) {
+                       for (int j = 1; j <= columnCount; j++) {
+                           System.out.println(meta_data.getColumnName(j) + " - " + result_set.getString(j) + "\t");
+                       }
+                       System.out.println("\n");
+                   }
+                   System.out.printf("запрос занял: %d мс\n", TimeUnit.NANOSECONDS.toMillis(duration));
+               }
+
+               //=====================
+               // commands via INSERT
+               //=====================
+               else if (sql_queries[i].toLowerCase().substring(0, 6).equals("insert")) {
+                   long start_time = System.nanoTime();
+
+                   rows_affected = statement.executeUpdate(sql_queries[i]);
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   System.out.println("Добавлено строк: " + rows_affected);
+
+                   System.out.printf("запрос занял: %d мс \n", TimeUnit.NANOSECONDS.toMillis(duration));
+               }
+
+
+               //=====================
+               // commands via DELETE
+               //=====================
+               else if (sql_queries[i].toLowerCase().substring(0, 6).equals("delete")) {
+                   long start_time = System.nanoTime();
+
+                   rows_affected = statement.executeUpdate(sql_queries[i]);
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   System.out.println("Удален строк: " + rows_affected);
+
+                   System.out.printf("запрос занял: %d мс \n", TimeUnit.NANOSECONDS.toMillis(duration));
+
+               }
+
+               //=====================
+               // commands via UPDATE
+               //=====================
+               else if (sql_queries[i].toLowerCase().substring(0, 6).equals("update")) {
+                   long start_time = System.nanoTime();
+
+                   rows_affected = statement.executeUpdate(sql_queries[i]);
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   System.out.println("обновлено строк: " + rows_affected);
+
+                   System.out.printf("запрос занял: %d мс \n", TimeUnit.NANOSECONDS.toMillis(duration));
+
+               }
+               //=====================
+               // commands via CREATE
+               //=====================
+               else if (sql_queries[i].toLowerCase().substring(0, 6).equals("create")) {
+                   long start_time = System.nanoTime();
+
+                   rows_affected = statement.executeUpdate(sql_queries[i]);
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   System.out.println("Таблица успешно создана!");
+                   System.out.printf("запрос занял: %d мс \n", TimeUnit.NANOSECONDS.toMillis(duration));
+
+               }
+
+               else {
+                   long start_time = System.nanoTime();
+
+                   rows_affected = statement.executeUpdate(sql_queries[i]);
+
+                   // get duration
+                   long end_time = System.nanoTime();
+                   long duration = end_time - start_time;
+
+                   System.out.println("Запрос успешно завершился: " + rows_affected);
+
+                   System.out.printf("запрос занял: %d мс \n", TimeUnit.NANOSECONDS.toMillis(duration));
+
+               }
+
+               System.out.println();
+
+           }
+
+       } catch (Exception e) {
+           System.err.println("[ERROR]: " + e);
+       }
+
+
+
+    }
 
 
 }
